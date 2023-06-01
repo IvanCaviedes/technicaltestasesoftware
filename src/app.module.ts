b88024ed3cad86089b9fdddd1a1140ccbdc71369
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-06-01 09:05:56 -0500

    fix/moviendo y eliminando arhivos test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from '../ormconfig';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { setEnvironment } from 'src/environment';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { setEnvironment } from 'src/infrastructure/environments';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: setEnvironment(), isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
