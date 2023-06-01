2392219394ad193ef65ea89bd48a4e5aee70e9fe
Ivan Caviedes <36604636+IvanCaviedes@users.noreply.github.com>
2023-06-01 08:42:31 -0500

    Feature/config database (#1)
    * feat/config eslint and prettier
    
    * feat/config database sql server
    
    * feat/add env file and environment function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from '../ormconfig';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { setEnvironment } from 'src/environment';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: setEnvironment(), isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
