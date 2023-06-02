aca76cea5abffb9098d7c465b256dbdd4a80933f
Ivan Caviedes <ivancaviedes99@gmail.com>
2023-06-02 00:16:05 -0500

    feat/ docs turn

--- src/presentation/controllers/turn.controller.ts
+++ src/presentation/controllers/turn.controller.ts
@@ -8,19 +8,42 @@ import {
   Query,
   Param,
 } from '@nestjs/common';
+
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiUnprocessableEntityResponse,
+  ApiParam,
+} from '@nestjs/swagger';
+
 import { TurnUseCases } from 'src/application/use-cases/TurnUseCase';
 import { TurnModel } from 'src/domain/models/Turn';
 
 import { PaginationResponseVM, QueryPaginationVM } from '../view-models/Common';
 import { CreateTurnVM, UpdateTurnVM } from 'src/presentation/view-models/Turn';
 
-import { DeleteResult } from 'typeorm';
+import { NotFoundError } from '../errors/NotFoundError';
+import { TurnVM } from '../view-models/Turn/TurnVM';
+import { BadRequestError } from '../errors/BadRequestError';
+import { UnprocessableEntityError } from '../errors/UnprocessableEntityError';
 
+@ApiTags('Turns')
 @Controller('api/turns')
 export class TurnController {
   constructor(private readonly turnUseCases: TurnUseCases) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'All turns with relations with business and services',
+  })
+  @ApiOkResponse({
+    description: 'All Services fetched.',
+    type: [PaginationResponseVM<TurnModel>],
+  })
   allServices(
     @Query() query: QueryPaginationVM,
   ): Promise<PaginationResponseVM<TurnModel>> {
@@ -28,34 +51,91 @@ export class TurnController {
   }
 
   @Get(':id')
-  turnById(@Param('id') idBusness: string): Promise<TurnModel> {
-    return this.turnUseCases.getOneTurnByField('id_servicio', idBusness);
+  @ApiOperation({
+    summary: 'Find one turn by id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'The turn id to search',
+  })
+  @ApiOkResponse({ description: 'Service founded.', type: TurnVM })
+  @ApiNotFoundResponse({
+    description: 'turn cannot be founded.',
+    type: NotFoundError,
+  })
+  async turnById(@Param('id') idTurn: string): Promise<TurnVM> {
+    const turnRespomse = await this.turnUseCases.getOneTurnByField(
+      'id_servicio',
+      idTurn,
+    );
+    return TurnVM.fromViewModel(turnRespomse);
   }
 
   @Post(':idService')
-  createService(
+  @ApiOperation({
+    summary: 'Create a turn',
+  })
+  @ApiCreatedResponse({ description: 'turn created.', type: TurnVM })
+  @ApiBadRequestResponse({
+    description: 'The request object doesn`t match the expected one',
+    type: BadRequestError,
+  })
+  @ApiUnprocessableEntityResponse({
+    description: 'Validation error while creating services',
+    type: UnprocessableEntityError,
+  })
+  async createService(
     @Body() turn: CreateTurnVM,
     @Param('idService') idService: string,
-  ): Promise<TurnModel> {
-    return this.turnUseCases.createTurn(
+  ): Promise<TurnVM> {
+    const turnRespomse = await this.turnUseCases.createTurn(
       CreateTurnVM.fromViewModel(turn),
       idService,
     );
+    return TurnVM.fromViewModel(turnRespomse);
   }
 
   @Patch(':id')
-  updateService(
+  @ApiOperation({
+    summary: 'Update an turn',
+  })
+  @ApiCreatedResponse({ description: 'turn updated.', type: TurnVM })
+  @ApiBadRequestResponse({
+    description: 'The request object doesn`t match the expected one',
+    type: BadRequestError,
+  })
+  @ApiUnprocessableEntityResponse({
+    description: 'Validation error while updating services',
+    type: UnprocessableEntityError,
+  })
+  async updateService(
     @Body() turn: UpdateTurnVM,
     @Param('id') idTurn: string,
-  ): Promise<TurnModel> {
-    return this.turnUseCases.updateTurn(
+  ): Promise<TurnVM> {
+    const turnRespomse = await this.turnUseCases.updateTurn(
       idTurn,
       UpdateTurnVM.fromViewModel(turn),
     );
+    return TurnVM.fromViewModel(turnRespomse);
   }
 
   @Delete(':id')
-  deleteTurn(@Param('id') idTurn: string): Promise<DeleteResult> {
-    return this.turnUseCases.deleteTurn(idTurn);
+  @ApiOperation({
+    summary: 'Delete a turn with id',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'The turn id',
+  })
+  @ApiOkResponse({ description: 'turn deleted.', type: TurnVM })
+  @ApiBadRequestResponse({
+    description: 'The request object doesn`t match the expected one',
+    type: BadRequestError,
+  })
+  async deleteTurn(@Param('id') idTurn: string): Promise<TurnVM> {
+    const turnRespomse = await this.turnUseCases.deleteTurn(idTurn);
+    return TurnVM.fromViewModel(turnRespomse);
   }
 }
--- src/presentation/view-models/Turn/CreateTurnVM.ts
+++ src/presentation/view-models/Turn/CreateTurnVM.ts
@@ -1,22 +1,26 @@
 import { IsOptional, IsNotEmpty, IsString, IsBoolean } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 import { TurnModel } from 'src/domain/models/Turn';
 
 export class CreateTurnVM {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   fecha_turno: Date;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   hora_inicio: Date;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   hora_fin: Date;
 
   @IsOptional()
   @IsBoolean()
+  @ApiProperty()
   estado?: boolean;
 
   static fromViewModel(vm: CreateTurnVM): TurnModel {
